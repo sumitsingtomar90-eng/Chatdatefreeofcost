85fb1e5aac5b9552b401641fb9de3dbb4e0fbf98
sumitsingtomar90-eng <sumitsingtomar90@gmail.com>
2025-12-05 00:07:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import '../screens/home.dart';
+import '../screens/login.dart';
+import '../screens/signup.dart';
+import '../screens/profile.dart';
+import '../screens/chat.dart';
+import '../screens/match.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,41 +15,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ChatDateFreeOfCost',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ChatDateFreeOfCost'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to ChatDateFreeOfCost!',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Sign Up'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Login'),
-            ),
-          ],
-        ),
-      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+        '/home': (context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/chat': (context) => ChatScreen(),
+        '/match': (context) => MatchScreen(),
+      },
     );
   }
 }
